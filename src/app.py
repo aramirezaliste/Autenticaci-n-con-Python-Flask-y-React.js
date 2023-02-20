f467754f4455846297d47be3f6635357c473d71e
aramirezaliste <aramirezaliste@gmail.com>
2023-02-20 23:07:32 +0000

    Avance ejercicio, Back autentificacion de registro y login

--- src/app.py
+++ src/app.py
@@ -7,10 +7,13 @@ from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from api.utils import APIException, generate_sitemap
-from api.models import db
+from api.models import db, User
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
+
+import datetime
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 
 #from models import Person
 
@@ -18,6 +21,10 @@ ENV = os.getenv("FLASK_ENV")
 static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../public/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
+#Integracion JWT
+app.config["JWT_SECRET_KEY"] = "super-secret"
+jwt = JWTManager(app)
 
 # database condiguration
 db_url = os.getenv("DATABASE_URL")
@@ -62,6 +69,58 @@ def serve_any_other_file(path):
     response = send_from_directory(static_file_dir, path)
     response.cache_control.max_age = 0 # avoid cache memory
     return response
+
+@app.route("/signup", methods=["POST"])  
+def signup():
+    body = request.get_json()
+    if "email" not in body:
+        print("email")
+        return "falta email", 400
+    if "password" not in body:
+        print("pass")
+        return "falta contraseña", 400
+
+    email = body["email"]
+    password = body["password"]
+
+    user = User(email=email,password=password,is_active=True)
+    db.session.add(user)
+    db.session.commit()
+  
+    return jsonify({"email":email, "password":password})
+
+@app.route('/login', methods=['POST'])
+def login():
+    #usuario contraseña :D
+    body = request.get_json()
+    if "email" not in body:
+        return "falta email", 400
+    if "password" not in body:
+        return "falta contraseña", 400
+
+    #validar datos, almacenar datos, mensaje de estado
+    email = body["email"]
+    password = body["password"]
+
+    user = User.query.filter_by(email = email, password = password).first()
+
+    if(user):
+    #otorgar permisos
+        expira = datetime.timedelta(minutes=3)
+        token_access = create_access_token(identity=email, expires_delta=expira)
+        return jsonify({"token": token_access, "email": email}), 200
+    else:
+        return jsonify({"msg": "Bad username or password"}), 401
+
+@app.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Accede a la identidad del usuario actual con get_jwt_identity
+    current_user_id = get_jwt_identity()
+    user = User.filter.get(current_user_id)
+    
+    return jsonify({"id": user.id, "email": user.email }), 200
+
 
 
 # this only runs if `$ python src/main.py` is executed
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -16,20 +16,44 @@ const getState = ({ getStore, getActions, setStore }) => {
 			]
 		},
 		actions: {
+			getToken: () => {
+				const login = async (username, password) => {
+					const resp = await fetch(`https://your_api.com/token`, {
+						method: "POST",
+						headers: { "Content-Type": "application/json" },
+						body: JSON.stringify({ username: "joe", password: "1234" })
+					})
+
+					if (!resp.ok) throw Error("There was a problem in the login request")
+
+					if (resp.status === 401) {
+						throw ("Invalid credentials")
+					}
+					else if (resp.status === 400) {
+						throw ("Invalid email or password format")
+					}
+					const data = await resp.json()
+					// save your token in the localStorage
+					//also you should set your user into the store using the setStore function
+					localStorage.setItem("jwt-token", data.token);
+
+					return data
+				}
+			},
 			// Use getActions to call a function within a fuction
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
 			},
 
 			getMessage: async () => {
-				try{
+				try {
 					// fetching data from the backend
 					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
 					const data = await resp.json()
 					setStore({ message: data.message })
 					// don't forget to return something, that is how the async resolves
 					return data;
-				}catch(error){
+				} catch (error) {
 					console.log("Error loading message from backend", error)
 				}
 			},
